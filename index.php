a95c9fc0aa76d499cbb4f168ca47fb4ecd8be615
shivam sharma <shivamsharma38391@gmail.com>
2023-06-16 17:26:50 +0530

    blog page setup

--- index.php
+++ index.php
@@ -1,154 +1,28 @@
 <?php echo get_header(); ?>
 
 
-      <!-- banner section start -->
-      <div class="banner_section layout_padding">
-        <div class="container-fluid">
-          <div class="row">
-            <div class="col-sm-8 padding_0">
-              <div class="banner_img"><img src="<?php echo get_template_directory_uri(); ?>/images/banner-img.png"></div>
-            </div>
-            <div class="col-sm-4">
-              <h1 class="clever_text">Clever <span style="color: #212122;">Investing</span> makes
-              Your Money Grow</h1>
-              <div class="read_bt_2"><a href="#">Read More</a></div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <!-- banner section end -->
-      <!-- services section start -->
-      <div class="services_section layout_padding">
-        <div class="container">
-          
-         <div class="d-flex  justify-content-center">
-          <h5 class="what_text">What we do</h5>
-         </div>
-          <div class="d-flex  justify-content-center">
-            <h1 class="services_text custom_main">Our Expertize & Services</h1>
-          </div>
-        </div>
-      </div>
-      <!-- services section end -->
-      <!-- services_2 section start -->
-      <div class="services_section_2">
-        <div class="container">
-          <div class="row">
-            <div class="col-sm-12 col-lg-8">
-              <div class="plane_main active">
-                <div class="plane_left">
-                  <div class="icon_1"><img src="<?php echo get_template_directory_uri(); ?>/images/icon-1.png"></div>
-                </div>
-                <div class="plane_right">
-                  <h3 class="vision_text">VISION TO PLAN</h3>
-                  <p class="completely_text">Completely synergize resource taxing relationships via niche markets. Completely synergize resource taxing.</p>
-                </div>
-              </div>
-              <div class="plane_main">
-                <div class="plane_left">
-                  <div class="icon_1"><img src="<?php echo get_template_directory_uri(); ?>/images/icon-2.png"></div>
-                </div>
-                <div class="plane_right">
-                  <h3 class="vision_text">GROWTH TO VISION</h3>
-                  <p class="completely_text">Completely synergize resource taxing relationships via niche markets. Completely synergize resource taxing.</p>
-                </div>
-              </div>
-              <div class="plane_main">
-                <div class="plane_left">
-                  <div class="icon_1"><img src="<?php echo get_template_directory_uri(); ?>/images/icon-3.png"></div>
-                </div>
-                <div class="plane_right">
-                  <h3 class="vision_text">MARKET TO GROWTH</h3>
-                  <p class="completely_text">Completely synergize resource taxing relationships via niche markets. Completely synergize resource taxing.</p>
-                </div>
-              </div>
-              <div class="plane_main">
-                <div class="plane_left">
-                  <div class="icon_1"><img src="<?php echo get_template_directory_uri(); ?>/images/icon-4.png"></div>
-                </div>
-                <div class="plane_right">
-                  <h3 class="vision_text">PLAN TO MARKET</h3>
-                  <p class="completely_text">Completely synergize resource taxing relationships via niche markets. Completely synergize resource taxing.</p>
-                </div>
-              </div>
-              <div class="read_bt"><a href="#">Read More</a></div>
-            </div>
-            <div class="col-sm-12 col-lg-4">
-              <div class="image_1"><img src="<?php echo get_template_directory_uri(); ?>/images/img-1.png"></div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <!-- services_2 section end -->
-      <!-- money section start -->
-      <div class="money_section">
-        <div class="container-fluid">
-          <div class="row">
-            <div class="col-sm-12 col-lg-4">
-              <div class="money_taital">
-                <h3 class="advanced_text">ADVANCED SERVICE</h3>
-                <h1 class="data_text">Data Analytics</h1>
-                <p class="lorem_text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it </p>
-                <div class="read_bt_1"><a href="#">Read More</a></div>
-              </div>
-            </div>
-            <div class="col-sm-12 col-lg-8 padding_0">
-              <div class="image_2"><img src="<?php echo get_template_directory_uri(); ?>/images/img-2.png"></div>
-              <h1 class="your_text">Your<br> Money<br> Grow</h1>
-            </div>
-          </div>
-        </div>
-      </div>
-      <!-- money section end -->
+
+
+<?php  
+
+  // this havepost function only work in index.php
+  while(have_posts()) {
+    
+    the_post();
+
+  ?>
+
+  <h2> <?php the_title() ?> </h2>
+
+  <p> <?php the_content() ?> </p>
+
+
+<?php  } ?>
+
+
       
-      <!-- client section start -->
-      <div class="client_section layout_padding">
-        <div class="container">
-          <div class="d-flex  justify-content-center">
-            <h1 class="services_text custom_main">Testimonial</h1>
-          </div>
-        </div>
-      </div>
-      <div class="client_section_2 layout_padding">
-        <div class="container">
-          <div id="main_slider" class="carousel slide" data-ride="carousel">
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-                <div class="client_main"><img src="<?php echo get_template_directory_uri(); ?>/images/img-3.png"></div>
-                <div class="client_main_1">
-                  <h1 class="kamlioo_text">kamlioo</h1>
-                  <p class="ipsum_text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it </p>
-                  <div class="quote_icon"><img src="images/quote-icon.png"></div>
-                </div>
-              </div>
-              <div class="carousel-item">
-                <div class="client_main"><img src="<?php echo get_template_directory_uri(); ?>/images/img-3.png"></div>
-                <div class="client_main_1">
-                  <h1 class="kamlioo_text">kamlioo</h1>
-                  <p class="ipsum_text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it </p>
-                  <div class="quote_icon"><img src="images/quote-icon.png"></div>
-                </div>
-              </div>
-              <div class="carousel-item">
-                <div class="client_main"><img src="<?php echo get_template_directory_uri(); ?>/images/img-3.png"></div>
-                <div class="client_main_1">
-                  <h1 class="kamlioo_text">kamlioo</h1>
-                  <p class="ipsum_text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it </p>
-                  <div class="quote_icon"><img src="<?php echo get_template_directory_uri(); ?>/images/quote-icon.png"></div>
-                </div>
-              </div>
-            </div>
-            <a class="carousel-control-prev" href="#main_slider" role="button" data-slide="prev">
-              <i class="fa fa-angle-left"></i>
-            </a>
-            <a class="carousel-control-next" href="#main_slider" role="button" data-slide="next">
-              <i class="fa fa-angle-right"></i>
-            </a>
-          </div>
-        </div>
-      </div>
-      <!-- client section end -->
-      
+
+
 
 
 <?php get_footer() ?>
